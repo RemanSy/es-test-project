d4275dd2b95fe0aaf6c94a92c4cf7f46a3ec168b
RemanSy <odamlord@mail.ru>
2022-03-08 19:32:53 +0300

    choices and conditions added

--- scripts/app.js
+++ scripts/app.js
@@ -2,38 +2,49 @@ let app = {
     filler : document.querySelector('#filler'),
     background : document.querySelector('#background'),
     char : document.querySelector('#char'),
+    choice : document.querySelector('#choice'),
     dialog : document.querySelector('#dialog'),
     name : document.querySelector('.name'),
     phrase : document.querySelector('.phrase'),
     arrowPrev : document.querySelector('#arrowPrev'),
     arrowNext : document.querySelector('#arrowNext'),
     audio : new Audio(''),
+    points : {},
     curFrame : 0,
     curQuote : 0,
     quotes : 0,
 
     start : function(frames) {
-        this.renderFrame(this.getCurFrame());
+        this.renderFrame(this.getFrame());
     },
 
     loadFrames : function(frames) {
         this.frames = frames;
     },
 
+    loadGroups : function(groups) {
+        this.groups = groups;
+    },
+
+    confColors : function(colors) {
+        this.colors = colors;
+    },
+
     next : function() {
         this.arrowPrev.style.display = 'block';
         this.curQuote += 1;
+        this.quotes = this.getFrame().quotes.length - 1;
 
         if (this.curQuote > this.quotes) {
             this.curFrame += 1;
-            this.renderFrame(this.getCurFrame());
+            this.renderFrame(this.getFrame());
         }
 
         if ((this.quotes < this.curQuote + 1) && (this.frames.length - 1 < this.curFrame + 1)) {
             this.arrowNext.style.display = 'none';
         }
 
-        this.renderPhrase(this.getCurQuote());
+        this.renderPhrase(this.getQuote());
     },
 
     previous : function() {
@@ -41,28 +52,32 @@ let app = {
         this.curQuote -= 1;
 
         if (this.curQuote < 0) {
-            this.curFrame -= 1
-            this.renderFrame(this.getCurFrame());
+            this.curFrame -= 1;
+            this.renderFrame(this.getFrame());
             this.curQuote = this.quotes;
         }
 
-        if ((this.curQuote - 1 < 0) && (this.curFrame - 1 < 0)) {
+        if (((this.curQuote - 1 < 0) && (this.curFrame - 1 < 0))) {
             this.arrowPrev.style.display = 'none';
         }
 
-        this.renderPhrase(this.getCurQuote());
+        this.renderPhrase(this.getQuote());
     },
 
-    getCurFrame : function() {
-        if (this.frames.length - 1 >= this.curFrame)
+    getFrame : function(num = null) {
+        if (num != null && this.frames.length - 1 >= num && num >= 0)
+            return this.frames[num];
+        else if (this.frames.length - 1 >= this.curFrame)
             return this.frames[this.curFrame];
         else
             return false;
     },
 
-    getCurQuote: function() {
-        if (this.quotes >= this.curQuote)
-            return this.getCurFrame().quotes[this.curQuote];
+    getQuote : function(num = null) {
+        if (num != null && this.quotes >= num && num >= 0)
+            return this.getFrame().quotes[num];
+        else if (this.quotes >= this.curQuote)
+            return this.getFrame().quotes[this.curQuote];
         else
             return false;
     },
@@ -71,6 +86,9 @@ let app = {
         this.curQuote = 0;
         this.quotes = frame.quotes.length - 1;
 
+        if (frame.audio.volume != 0 || frame.audio.volume != 0)
+            sleep(300);
+
         if (frame.audio && new Audio('sound/' + frame.audio).src != this.audio.src) {
             this.audio.smoothPause();
             sleep(100);
@@ -78,15 +96,84 @@ let app = {
             this.audio.setAttribute('loop', '');
             this.audio.smoothPlay();
         }
-        
+
         this.changeBackground(frame.background);
         this.changeChar(frame.character);
-        this.renderPhrase(this.getCurQuote());
+        this.renderPhrase(this.getQuote());
     },
 
     renderPhrase : function(phrase) {
-        this.name.innerText = phrase[0];
-        this.phrase.innerText = phrase[1];
+        console.log(phrase);
+        console.log(this.curQuote, this.quotes);
+        if (phrase[0] == CHOICE) {
+            this.choice.style.display = 'flex';
+            this.dialog.style.display = 'none';
+            this.chsVarnts = phrase[2];
+            for (let key in phrase[2])
+                this.choice.querySelector('ul').append(createNode(`<li>${key}</li>`));
+                
+        } else if (phrase[0] == CONDITION) {
+            this.resolveCondition(phrase);
+        } else {
+            this.name.innerText = phrase[0];
+            this.phrase.innerText = phrase[1];
+            this.name.style.color = this.colors[phrase[0]] ?? '#fff';
+        }
+    },
+
+    resolveChoice : function(text) {
+        for (let key in this.chsVarnts[text]) {
+            if (this.points.hasOwnProperty(key)) this.points[key] += this.chsVarnts[text][key];
+            else this.points[key] = this.chsVarnts[text][key];
+        }
+        this.renderPhrase([this.getQuote()[1], text]);
+        this.getFrame().quotes[this.curQuote] = [this.getQuote()[1], text];
+
+        this.choice.querySelector('ul').innerHTML = '';
+        this.choice.style.display = 'none';
+        this.dialog.style.display = 'block';
+    },
+
+    resolveCondition : function(cond) {
+        switch(cond[1][1]) {
+            case '>':
+                if (this.points[cond[1][0]] > cond[1][2]) {
+                    this.frames.splice(this.curFrame + 1, 10000);
+                    this.groups[cond[2]].forEach(frame => this.frames.push(frame));
+                    this.getFrame().quotes.splice(this.curQuote, 1000);
+                    this.next();
+                } else {
+                    this.getFrame().quotes.splice(this.curQuote, 1);
+                    this.renderPhrase(this.getQuote);
+                }
+                break;
+
+            case '<':
+                if (this.points[cond[1][0]] < cond[1][2]) {
+                    this.frames.splice(this.curFrame + 1, 10000);
+                    this.groups[cond[2]].forEach(frame => this.frames.push(frame));
+                    this.getFrame().quotes.splice(this.curQuote, 1000);
+                    this.next();
+                } else {
+                    this.getFrame().quotes.splice(this.curQuote, 1);
+                    this.renderPhrase(this.getQuote());
+                }
+                
+                break;
+
+            case '=':
+                if (this.points[cond[1][0]] == cond[1][2]) {
+                    this.frames.splice(this.curFrame + 1, 10000);
+                    this.groups[cond[2]].forEach(frame => this.frames.push(frame));
+                    this.getFrame().quotes.splice(this.curQuote, 1000);
+                    this.next();
+                } else {
+                    this.getFrame().quotes.splice(this.curQuote, 1);
+                    this.renderPhrase(this.getQuote());
+                }
+
+                break;
+        }
     },
 
     changeBackground : function(path) {
@@ -110,6 +197,9 @@ let app = {
         }
     }
 }
+
+const CHOICE = 'cniyec19n91438nc1nfc';
+const CONDITION = '0n9acponifq42nu8rsih';
 
 window.onload = () => {
     app.char.width = app.contain({width : 1920, height : 1080}, {width : window.innerWidth, height : window.innerHeight}).width;
@@ -136,15 +226,31 @@ app.arrowNext.addEventListener('click', () => app.next());
 
 app.arrowPrev.addEventListener('click', () => app.previous());
 
+app.choice.addEventListener('click', e => { if (e.target.tagName == 'LI') app.resolveChoice(e.target.innerText) });
+
+function createNode(html) {
+    let tmp = document.createElement('template');
+    html = html.trim();
+    tmp.innerHTML = html;
+    return tmp.content.firstChild;
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 Audio.prototype.smoothPause = async function() {
-    for (let i = 0; i < 97; i++) {
-        await sleep(21);
-        this.volume -= 0.01;
+    try {
+        for (let i = 0; i < 98; i++) {
+            if (this.volume == 0) break
+            await sleep(21);
+            this.volume -= 0.01;
+        }
+    } catch(e) {
+        this.volume = 0;
     }
+
+    this.volume = 0;
     this.muted = true;
     this.pause();
 }
@@ -153,9 +259,11 @@ Audio.prototype.smoothPlay = async function() {
     this.muted = false;
     this.volume = 0.01;
     this.play();
-    await sleep(700);
+    // await sleep(700);
     for (let i = 0; i < 98; i++) {
+        if (this.volume == 0) break
         await sleep(31);
         this.volume += 0.01;
     }
+    this.volume = 1;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,13 @@ app.loadFrames([
             ['Лена', 'Привет'],
             ['Лена', 'Я Лена, вскрываю вены)'],
             ['Лена', 'А ты?'],
+            [CHOICE, 'Semen', {
+                'А я Борис Америка' : {'Лена' : -5},
+                'А я кидаю насвай' : {'Лена' : 100, 'МВД' : -50},
+                'А я Семён' : {'Лена' : -100},
+            }],
+            [CONDITION, ['Лена', '=', 100], 'LenaRoot'],
+            [CONDITION, ['Лена', '=', -5], 'LenaBad'],
         ],
     },
 
@@ -29,11 +36,48 @@ app.loadFrames([
     },
 
     {
-        background : 'grey-texture-background-desktop-3.jpg',
+        background : 'lizard-dance.gif',
         character : 'none.png',
         audio : 'outro.mp3',
         quotes : [
             ['Разработчик', 'Всё, конец.'],
         ],
-    }
+    },
 ]);
+
+app.loadGroups({
+    'LenaRoot' : [{
+                    background : 'EverlastingSummer-Background (112).jpg',
+                    character : 'none.png',
+                    audio : '41 - Miku Song (Flute).ogg',
+                    quotes : [
+                        ['Semen', 'Что происходит? Я опять куда-то телепортировался?'],
+                    ],
+                },
+                {
+                    background : 'EverlastingSummer-Background (112).jpg',
+                    character : 'Sprite (193).png',
+                    audio : '41 - Miku Song (Flute).ogg',
+                    quotes : [
+                        ['Лена', 'Семён, ты охуенен. Вот тебе положительная концовка'],
+                    ],
+                },
+            ],
+
+    'LenaBad' : [{
+                    background : 'EverlastingSummer-Background (92).jpg',
+                    character : 'Sprite (171)asdas.png',
+                    audio : '32 - Miku\'s Song.ogg',
+                    quotes : [
+                        ['Лена', 'Пизда тебе Семён'],
+                        ['Semen', 'За шо? НЕЕЕЕЕЕЕЕЕЕТ'],
+                    ],
+                },
+            ],
+});
+
+app.confColors({
+    'Лена' : '#9900FF',
+    'Semen' : '#f4f4f4',
+    'Разработчик' : 'magenta',
+});
